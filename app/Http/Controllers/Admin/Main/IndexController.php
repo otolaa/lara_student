5de8927e1ebb1b6c27488a145603180a522de563
otolaa <kalabunga82@gmail.com>
2023-10-11 22:32:11 +0200

    🎲

--- app/Http/Controllers/Admin/Main/IndexController.php
+++ app/Http/Controllers/Admin/Main/IndexController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Admin\Main;
 
 use App\Http\Controllers\Controller;
+use App\Models\Sgcwt;
+use App\Models\User;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        return view('admin.main.index');
+        $user_count = User::all()->count();
+        $sgcwt_count = Sgcwt::all()->count();
+        return view('admin.main.index', compact('user_count', 'sgcwt_count'));
     }
 }
